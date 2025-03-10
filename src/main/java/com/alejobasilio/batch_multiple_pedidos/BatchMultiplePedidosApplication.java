aaea3f08dc1879b5d8a7da55d6a4382677d770ad
alejo.basilio <alejo.basilio@viewnext.com>
2025-03-10 10:42:05 +0100

    commit inicial

---
+++ src/main/java/com/alejobasilio/batch_multiple_pedidos/BatchMultiplePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.alejobasilio.batch_multiple_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMultiplePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchMultiplePedidosApplication.class, args);
+	}
+
+}
