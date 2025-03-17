a8368213a9588e94e1fd7a5f8b14c975c69b8a76
alejo.basilio <alejo.basilio@viewnext.com>
2025-03-17 08:43:33 +0100

    models comentado

--- src/main/java/com/alejobasilio/batch_multiple_pedidos/model/Pedido.java
+++ src/main/java/com/alejobasilio/batch_multiple_pedidos/model/Pedido.java
@@ -7,6 +7,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Clase que representa un pedido, con atributos para id, nombre del cliente, id_producto, importe, estado y valido.
+ * Proporciona constructores y métodos getter y setter para acceder y modificar los atributos.
+ * 
+ * @author Alejo Basilio Alfonso
+ * @version 1.0
+ * @since 1.0
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
--- src/main/java/com/alejobasilio/batch_multiple_pedidos/model/Producto.java
+++ src/main/java/com/alejobasilio/batch_multiple_pedidos/model/Producto.java
@@ -7,6 +7,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Clase que representa un producto, con atributos para id, nombre del producto, precio y moneda.
+ * Proporciona constructores y métodos getter y setter para acceder y modificar los atributos.
+ * 
+ * @author Alejo Basilio Alfonso
+ * @version 1.0
+ * @since 1.0
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
